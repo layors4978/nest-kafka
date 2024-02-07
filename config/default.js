9b9abb816e855632b9929889ba8c3874513afb87
layorschou <layorschou@oringnet.com>
2024-02-07 15:55:20 +0800

    chore: add kafka brokers env var

--- config/default.js
+++ config/default.js
@@ -6,4 +6,8 @@ module.exports = {
     host: 'localhost',
     port: 3000,
   },
+
+  kafka: {
+    brokers: '["kafka-1:19092","kafka-2:19093","kafka-3:19094"]',
+  },
 };
--- src/env.ts
+++ src/env.ts
@@ -4,3 +4,7 @@ export const VERSION = config.get('version') as string;
 
 export const HTTP_HOST = config.get('http.host') as string;
 export const HTTP_PORT = config.get('http.port') as number;
+
+export const KAFKA_BROKERS = JSON.parse(
+  config.get('kafka.brokers'),
+) as string[];
